c521c9f0535255e68998915b320cd02b6607cc03
Tory Rachel Van Tassel <countrygirlr09@hotmail.com>
2021-09-09 18:13:24 -0600

    wip

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('web 29 rocks!')
+require('dotenv').config();
+
+const { PORT } = require('./config');
+
+const express = require('express');
+
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Web 29 is awesome!</h1>
+    `)
+});
+
+
+server.use('*', (req, res) => {
+    res.json({
+        message: 'web 29 is awesome!'
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
